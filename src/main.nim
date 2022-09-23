c9ddd1f29ffa81e98728abd12a3e41fdc7d4e1c9
Johannes Maier <johannes.maier@active-group.de>
2022-09-23 17:09:41 +0200

    Rotate the triangle in steps

--- src/main.nim
+++ src/main.nim
@@ -1,3 +1,4 @@
+import std/math as math
 import ncurses
 import vmath
 
@@ -11,13 +12,19 @@ proc main() =
   var x, y: cint
   getmaxyx(w, y, x)
 
-  let transformation = scale(vec3(4,2,1))
-  var v1 = transformation * vec4(10, 10, 1, 1)
-  var v2 = vec4(20, 10, 1, 1)
-  var v3 = vec4(15, 20, 1, 1)
-
   let rasterizer = Rasterizer(height: y, width: x)
-  rasterizer.rasterizeTriangle(vec2(v1.x, v1.y), vec2(v2.x, v2.y), vec2(v3.x, v3.y))
+
+  var angle: float32 = 0
+  while angle < 360:
+    let transformation = rotate(math.degToRad(angle), vec3(0, 0, 1))
+    var v1 = transformation * vec4(10, 10, 1, 1)
+    var v2 = vec4(20, 10, 1, 1)
+    var v3 = vec4(15, 20, 1, 1)
+
+    rasterizer.rasterizeTriangle(vec2(v1.x, v1.y), vec2(v2.x, v2.y), vec2(v3.x, v3.y))
+    getch()
+    clear()
+    angle += 5
 
   getch()
 
